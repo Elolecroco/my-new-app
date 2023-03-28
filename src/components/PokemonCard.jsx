6944b3d6ab789b3b110e293893b4b29ca4f69160
ELOISE BROCHIER <eloise.brochier@gmail.com>
2023-03-28 11:52:56 +0200

    pokemonlist

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,8 +1,19 @@
 function MyPokemonCard() {
-    return <figure>
-        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="bulbasaur"/>
-        <figcaption>Bulbasaur</figcaption>
-    </figure>;
-  }
-  
-  export default MyPokemonCard;
+    const pokemon = pokemonList[0]
+    return (
+        pokemon.imgSrc ? <figure> 
+        <img src={pokemon.imgSrc} alt={pokemon.name}/> <figcaption>{pokemon.name}</figcaption>
+        </figure> : <figure><p>???</p> <figcaption>{pokemon.name}</figcaption></figure>)
+}
+
+const pokemonList = [
+    {
+        name: "Bulbasaur",
+        imgSrc: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+        name: "Mew"
+    }
+]
+
+export default MyPokemonCard;
