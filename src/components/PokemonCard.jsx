ced3b3c8963a77096a5fc739f12f93cf7cbe97ce
ELOISE BROCHIER <eloise.brochier@gmail.com>
2023-03-28 10:39:57 +0200

    bulbasaur git add .

---
+++ src/components/PokemonCard.jsx
@@ -0,0 +1,8 @@
+function MyPokemonCard() {
+    return <figure>
+        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="bulbasaur"/>
+        <figcaption>Bulbasaur</figcaption>
+    </figure>;
+  }
+  
+  export default MyPokemonCard;
