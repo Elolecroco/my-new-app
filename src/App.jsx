9f58fcb3fd7706e021f3d04a68bb93439a471b1a
ELOISE BROCHIER <eloise.brochier@gmail.com>
2023-03-28 15:38:32 +0200

    proptypes

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import PropTypes from "prop-types"
 import MyPokemonCard from './components/PokemonCard'
 
 const pokemonList = [
--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,8 +1,17 @@
+import PropTypes from "prop-types"
+
 function MyPokemonCard({ pokemon }) {
     return (
         pokemon.imgSrc ? <figure> 
         <img src={pokemon.imgSrc} alt={pokemon.name}/> <figcaption>{pokemon.name}</figcaption>
         </figure> : <figure><p>???</p> <figcaption>{pokemon.name}</figcaption></figure>)
+}
+
+MyPokemonCard.propTypes = {
+    pokemon: PropTypes.shape({
+        name: PropTypes.string.isRequired,
+        imgSrc: PropTypes.string,
+    }).isRequired,
 }
 
 export default MyPokemonCard;
