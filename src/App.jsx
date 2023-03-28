9cc0e59e433e97b8d583542df6ad41ba952f4c79
ELOISE BROCHIER <eloise.brochier@gmail.com>
2023-03-28 15:16:32 +0200

    props and pokemon

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,20 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import MyPokemonCard from './components/PokemonCard'
 
+const pokemonList = [
+    {
+        name: "Bulbasaur",
+        imgSrc: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+        name: "Mew"
+    }
+];
+
 function App() {
-  return (
-    <div>
-      <MyPokemonCard />
-    </div>
-  );
+  return <MyPokemonCard pokemon={pokemonList[0]} />
 }
+
+
 
 export default App
--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,19 +1,8 @@
-function MyPokemonCard() {
-    const pokemon = pokemonList[0]
+function MyPokemonCard({ pokemon }) {
     return (
         pokemon.imgSrc ? <figure> 
         <img src={pokemon.imgSrc} alt={pokemon.name}/> <figcaption>{pokemon.name}</figcaption>
         </figure> : <figure><p>???</p> <figcaption>{pokemon.name}</figcaption></figure>)
 }
-
-const pokemonList = [
-    {
-        name: "Bulbasaur",
-        imgSrc: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-    },
-    {
-        name: "Mew"
-    }
-]
 
 export default MyPokemonCard;
